a5fef4d772ce221dc75b0825e0744dc4b3416e76
BT <bt@alifacademy.tj>
2020-02-21 22:33:18 +0500

    feat: added flags -cmd [command] -file [fileName]

--- fileExchange/client/client.go
+++ fileExchange/client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -11,6 +12,10 @@ import (
 	"servers/fileExchange/pkg/operations"
 	"strings"
 )
+
+var cmd = flag.String("cmd", "download", "command:")
+
+var arg = flag.String("file", "l2.bot", "file name:")
 
 func main() {
 	const addr = "localhost:9999"
@@ -30,51 +35,39 @@ func client(dial net.Conn) {
 			log.Printf("can't close connect , it error: %v", err)
 		}
 	}()
-	var cmd string
-	log.Print(operations.ClientCommand)
-	_, err := fmt.Scan(&cmd)
-	if err != nil {
-		log.Fatalf("can't read command: %s, it error: %v", cmd, err)
-	}
-	cmd = strings.ToLower(cmd)
-	switch cmd {
+	flag.Parse()
+	*cmd = strings.ToLower(*cmd)
+	switch *cmd {
 	case "download":
 		{
-			err := downloadClient(dial, cmd)
+			err := downloadClient(dial, *cmd)
 			if err != nil {
 				log.Printf("can't download file from server, it error: %v", err)
 			}
 		}
 	case "upload":
 		{
-			err := uploadClient(dial, cmd)
+			err := uploadClient(dial, *cmd)
 			if err != nil {
 				log.Printf("can't upload file from client: %v", err)
 			}
 		}
 	case "list":
 		{
-			err := listClient(dial, cmd)
+			err := listClient(dial, *cmd)
 			if err != nil {
 				log.Printf("can't get files list from server: %v", err)
 			}
 		}
 	default:
-		fmt.Printf("incorrect command: %s\n", cmd)
+		fmt.Printf("incorrect command: %s\n", *cmd)
 		return
 	}
 }
 
 func downloadClient(dial net.Conn, cmd string) (err error) {
 	cmd = cmd + ":"
-	arg := ""
-	fmt.Printf("Enter file name: ")
-	_, err = fmt.Scan(&arg)
-	if err != nil {
-		log.Fatalf("can't read command: %v", err)
-		return err
-	}
-	cmd = cmd + arg
+	cmd = cmd + *arg
 	cmdSend := bufio.NewWriter(dial)
 	log.Printf("try send command to server: %s: ", cmd)
 	err = operations.WriteLine(cmd, cmdSend)
@@ -83,9 +76,9 @@ func downloadClient(dial net.Conn, cmd string) (err error) {
 		return err
 	}
 	log.Println("command send success")
-	log.Printf("try download file: %s from server", arg)
+	log.Printf("try download file: %s from server", *arg)
 	reader := bufio.NewReader(dial)
-	_, err = operations.ReadLine(reader)
+	line, err := operations.ReadLine(reader)
 	if err != nil {
 		log.Printf("can't read: %v", err)
 		return err
@@ -97,13 +90,13 @@ func downloadClient(dial net.Conn, cmd string) (err error) {
 			return err
 		}
 	}
-	check := string(bytes)
-	log.Printf("check file: %s exist in server", arg)
-	if check == "error\n" {
-		log.Printf("file: %s is not exist in server", arg)
+	check := line
+	log.Printf("check file: %s exist in server", *arg)
+	if check == "error file not found\n" {
+		log.Printf("file: %s is not exist in server", *arg)
 		return err
 	}
-	log.Printf("file: %s exist in server", arg)
+	log.Printf("file: %s exist in server", *arg)
 	downloadDir := "./download/"
 	log.Printf("check dir status")
 	_, err = os.Stat(downloadDir)
@@ -115,14 +108,13 @@ func downloadClient(dial net.Conn, cmd string) (err error) {
 			return err
 		}
 		log.Printf("create directory success: %s", downloadDir)
-		return err
 	}
 	log.Printf("download file size by bytes: %d", len(bytes))
-	downloadFile := downloadDir + arg
-	log.Printf("try create file: %s", arg)
+	downloadFile := downloadDir + *arg
+	log.Printf("try create file: %s", *arg)
 	file, err := os.OpenFile(downloadFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
 	if err != nil {
-		log.Printf("can't create file: %s, error: %v", arg, err)
+		log.Printf("can't create file: %s, error: %v", *arg, err)
 		return err
 	}
 	defer func() {
@@ -132,28 +124,21 @@ func downloadClient(dial net.Conn, cmd string) (err error) {
 			return
 		}
 	}()
-	log.Printf("create file: %s success", arg)
-	log.Printf("try write to file: %s", arg)
+	log.Printf("create file: %s success", *arg)
+	log.Printf("try write to file: %s", *arg)
 	_, err = file.Write(bytes)
 	if err != nil {
-		log.Printf("can't write to file: %s, error: %v", arg, err)
+		log.Printf("can't write to file: %s, error: %v", *arg, err)
 		return err
 	}
-	log.Printf("file: %s, download success!", arg)
+	log.Printf("file: %s, download success!", *arg)
 	return nil
 }
 
 func uploadClient(dial net.Conn, cmd string) (err error) {
 	log.Printf("You need have directory \"upload\" and put there your files.")
 	cmd = cmd + ":"
-	arg := ""
-	fmt.Printf("Enter file name: ")
-	_, err = fmt.Scan(&arg)
-	if err != nil {
-		log.Fatalf("can't read command: %v", err)
-		return err
-	}
-	cmd = cmd + arg
+	cmd = cmd + *arg
 	cmdSend := bufio.NewWriter(dial)
 	log.Printf("try send command to server: %s: ", cmd)
 	err = operations.WriteLine(cmd, cmdSend)
@@ -162,31 +147,31 @@ func uploadClient(dial net.Conn, cmd string) (err error) {
 		return err
 	}
 	log.Println("command send success")
-	log.Printf("try upload file: %s to server", arg)
+	log.Printf("try upload file: %s to server", *arg)
 	writer := bufio.NewWriter(dial)
 	err = operations.WriteLine("upload: ok", writer)
 	if err != nil {
 		log.Printf("error while writing: %v", err)
 		return err
 	}
-	arg = strings.TrimSuffix(arg, "\n")
+	*arg = strings.TrimSuffix(*arg, "\n")
 	uploadDir := "./upload/"
-	uploadFile := uploadDir + arg
+	uploadFile := uploadDir + *arg
 	file, err := os.Open(uploadFile)
 	if err != nil {
-		log.Printf("can't find file: %s, it error: %v", arg, err)
+		log.Printf("can't find file: %s, it error: %v", *arg, err)
 		_ = operations.WriteLine("error", writer)
 		return err
 	}
-	log.Printf("try copy file: %s, from directory: %v", arg, uploadDir)
+	log.Printf("try copy file: %s, from directory: %v", *arg, uploadDir)
 	byteFile, err := io.Copy(writer, file)
 	if err != nil {
-		log.Printf("can't copy file: %s, it error: %v", arg, err)
+		log.Printf("can't copy file: %s, it error: %v", *arg, err)
 		return err
 	}
-	log.Printf("copy file: %s, size by bytes: %d", arg, byteFile)
+	log.Printf("copy file: %s, size by bytes: %d", *arg, byteFile)
 	log.Printf("file copy success")
-	log.Printf("file: %s send to server success", arg)
+	log.Printf("file: %s send to server success", *arg)
 	return nil
 }
 
